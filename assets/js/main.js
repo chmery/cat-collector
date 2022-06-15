18377c6d47e083c356c69f66b74113cda5ec2039
chmery <104914995+chmery@users.noreply.github.com>
2022-06-15 15:24:32 +0200

    Create remove functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,37 +2,62 @@ const drawnImagesContainer = document.querySelector(".container__draw-image");
 const addToCollectionBtn = document.querySelector("#add-to-collection-button");
 const drawAnotherBtn = document.querySelector("#draw-another-button");
 
+const collectionElementsAmountOutput = document.querySelector("#collection-elements-amount");
 const collectionImagesContainer = document.querySelector(".container__collection-images");
-const collectionImageTemplate = document.querySelector(".container__collection-template");
+const collectionElementTemplate = document.querySelector(".container__collection-template");
 
-let newImageSource = "";
-let templateImageSource = "";
+let drawnImageSource = "";
+let collectionElementsAmount = 0;
+let currentCollectionElementImageSource = "";
+let collectionElementId = 0;
 
-const setNewImage = () => {
+const setNewImageSource = () => {
     fetch("https://api.thecatapi.com/v1/images/search")
         .then((response) => response.json())
         .then((data) => {
-            newImageSource = data[0].url;
-            drawnImagesContainer.style.backgroundImage = `url(${newImageSource})`;
+            drawnImageSource = data[0].url;
+            drawnImagesContainer.style.backgroundImage = `url(${drawnImageSource})`;
         });
 };
 
 const addToCollection = () => {
-    const templateContent = collectionImageTemplate.content.cloneNode(true);
-    templateContent.querySelector(".container__collection-image").style.backgroundImage = `url("${newImageSource}")`;
+    const collectionElement = collectionElementTemplate.content.cloneNode(true);
+    collectionElement.querySelector(".container__collection-image").style.backgroundImage = `url("${drawnImageSource}")`;
+    collectionElement.querySelector(".container__collection-image").setAttribute("id", collectionElementId);
+    collectionElement.querySelector("#remove-image").setAttribute("onclick", `removeCollectionElement(${collectionElementId})`);
 
-    if (templateImageSource != `url("${newImageSource}")`) {
-        templateImageSource = `url("${newImageSource}")`;
-        collectionImagesContainer.appendChild(templateContent);
+    if (currentCollectionElementImageSource != `url("${drawnImageSource}")`) {
+        currentCollectionElementImageSource = `url("${drawnImageSource}")`;
+        collectionElementId++;
+        collectionElementsAmount++;
+        setCollectionElementsAmount();
+        collectionImagesContainer.appendChild(collectionElement);
     } else {
         alert("You've already added this cat!");
     }
 };
 
-setNewImage();
+const removeCollectionElement = (collectionElementId) => {
+    const collectionElementToRemove = document.getElementById(collectionElementId);
+    collectionElementToRemove.remove();
+    collectionElementsAmount--;
+    setCollectionElementsAmount();
+    checkIfCanAddAgain(collectionElementToRemove);
+};
 
-drawAnotherBtn.addEventListener("click", setNewImage);
+const checkIfCanAddAgain = (collectionElementToRemove) => {
+    if (collectionElementToRemove.style.backgroundImage == currentCollectionElementImageSource)
+        currentCollectionElementImageSource = "";
+};
+
+const setCollectionElementsAmount = () => {
+    collectionElementsAmountOutput.textContent = collectionElementsAmount;
+};
+
+//setNewImageSource();
+
+drawAnotherBtn.addEventListener("click", setNewImageSource);
 addToCollectionBtn.addEventListener("click", () => {
-    if (newImageSource == "") return;
+    if (drawnImageSource == "") return;
     addToCollection();
 });
