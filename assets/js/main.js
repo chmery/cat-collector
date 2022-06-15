895068e09d36754afd6fb305972a63e8d7dc6c43
chmery <104914995+chmery@users.noreply.github.com>
2022-06-15 16:56:40 +0200

    Create loading animation and script

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,23 @@ let collectionElementsAmount = 0;
 let currentCollectionElementImageSource = "";
 let collectionElementId = 0;
 
+const toggleLoader = (option) => {
+    const loader = document.querySelector(".container__loader");
+    if (option == "on") {
+        loader.style.display = "block";
+    } else {
+        loader.style.display = "none";
+    }
+};
+
 const setNewImageSource = () => {
+    toggleLoader("on");
     fetch("https://api.thecatapi.com/v1/images/search")
         .then((response) => response.json())
         .then((data) => {
             drawnImageSource = data[0].url;
             drawnImagesContainer.style.backgroundImage = `url(${drawnImageSource})`;
+            toggleLoader("off");
         });
 };
 
@@ -46,8 +57,7 @@ const removeCollectionElement = (collectionElementId) => {
 };
 
 const checkIfCanAddAgain = (collectionElementToRemove) => {
-    if (collectionElementToRemove.style.backgroundImage == currentCollectionElementImageSource)
-        currentCollectionElementImageSource = "";
+    if (collectionElementToRemove.style.backgroundImage == currentCollectionElementImageSource) currentCollectionElementImageSource = "";
 };
 
 const setCollectionElementsAmount = () => {
